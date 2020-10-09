bc9e36326bde87f2b2aacbe01733ce7ab12a06e5
Chintan Prajapati <chintanprajapati37@gmail.com>
2020-10-09 15:48:25 +0530

    tab navigation added

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home/HomeScreen';
 import DetailScreen from '../screens/Details/DetailScreen';
+
+const Tab = createBottomTabNavigator();
+
+const DashboardNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Details" component={DetailScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const Stack = createStackNavigator();
 
@@ -9,7 +21,7 @@ const AppNavigation = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={DetailScreen} />
+      <Stack.Screen name="Details" component={DashboardNavigation} />
     </Stack.Navigator>
   );
 };
